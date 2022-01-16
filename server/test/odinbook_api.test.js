e763c54c0138e93e50ece8f28b2a5c042116c80e
kazumaneet <mekbibtare@gmail.com>
2022-01-16 11:22:39 +0300

    Wrote tests for checking if it gives the correct response when given missing content

--- server/test/odinbook_api.test.js
+++ server/test/odinbook_api.test.js
@@ -17,27 +17,41 @@ describe('Creating user', () => {
     username:"kazuma",
     password:"thePower"
   }
-  test("User is succesfully created when all required restrictions are met", async () => {
+  const userWithNoName = {
+    username:"kazuma",
+    password:"thePower"
+  }
+  const userWithNoUserName = {
+    name:"Mekbib",
+    password:"thePower"
+  }
+
+  test("User is succesfully created when all required attributes are given", async () => {
     await api.post('/OdinBook/user')
     .send(user)
     .expect(201)
-    .expect('Content-Type',/application\/json/)
 
     const usersInDb  = await User.find({})
 
     expect(usersInDb).toHaveLength(1)
   },15000)
+
+  test("User is not created when required attributes are not given and\
+   we recieve a proper status code", async()=>{
+    await api.post('/OdinBook/user')
+    .send(userWithNoName)
+    .expect(400)
+
+    await api.post('/OdinBook/user')
+      .send(userWithNoUserName)
+      .expect(400)
+
+    const usersInDb = await User.find({})
+
+    expect(usersInDb).toHaveLength(0)
+  },15000)
 })
-// describe('Logging in', () => {
-//   beforeEach(async () => {
-//     const user = {
-//       name:Mekbib,
-//       password:the
-//     }
-//     User.deleteMany({})
-    
-//   })
-// })
+
 afterAll(() => {
   mongoose.connection.close()
 })

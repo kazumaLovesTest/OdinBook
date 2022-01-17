a8fddefe3c0bd31ef5b487b2090a0249fb17f071
kazumaneet <mekbibtare@gmail.com>
2022-01-17 21:02:36 +0300

    Wrote tests for loggin in

--- server/test/odinbook_api.test.js
+++ server/test/odinbook_api.test.js
@@ -77,6 +77,41 @@ describe('Creating user', () => {
   })
 })
 
+describe("Logging in", () =>{
+  beforeEach(async() =>{
+    const user = {
+      name: "Mekbib",
+      username: "kazuma",
+      password: "thePower"
+    }
+    await User.deleteMany({})
+    await api.post('/OdinBook/user')
+             .send(user)
+             .expect(201)
+  },15000)
+
+  const user = {
+    name: "Mekbib",
+    username: "kazuma",
+    password: "thePower"
+  }
+  const userWithWrongPassword = {
+    name: "Mekbib",
+    username: "kazuma",
+    password: "Water"
+  }
+  test("Login is succussful if given correct credintials", async() =>{
+    await api.post('/OdinBook/login')
+             .send(user)
+             .expect(200)
+  },15000)
+  test("Login fails if given incorrect credintials and returns proper status code",async() =>{
+    await api.post('/OdinBook/login')
+             .send(userWithWrongPassword)
+             .expect(401)
+  })
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })

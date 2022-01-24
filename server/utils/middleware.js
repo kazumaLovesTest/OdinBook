8b4f983641356d0ede2ed83dcb8d7f5e47a41a2b
kazumaneet <mekbibtare@gmail.com>
2022-01-24 19:57:36 +0300

    added a userExtractor and token extractor middleware

--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -1,5 +1,7 @@
-const handleValidationError = (error,req,res,next) => {
-  console.log(error.name)
+const JWT = require('jsonwebtoken')
+const User = require('../model/user')
+
+const handleValidationError = (error, req, res, next) => {
   if (error.name === 'ValidationError')
     return res.status(400).json({
       error: error.message
@@ -7,4 +9,29 @@ const handleValidationError = (error,req,res,next) => {
   next()
 }
 
-module.exports = {handleValidationError}
+const tokenExtractor = (req, res, next) => {
+  const authorization =  req.get('Authorization');
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer '))
+      req.token = authorization.substring(7)
+  else
+    req.token = null
+  next()
+}
+
+const userExtractor = async(req,res,next) => {
+  const token = req.token
+  const decodedToken =  JWT.verify(token,process.env.SECRET)
+
+  if (token === null || !decodedToken)
+    res.status(401).json({
+      error:error.message,
+    })
+  
+  const user = await User.findOne(decodedToken)
+
+  req.user = user
+
+  next()
+}
+module.exports = { handleValidationError,tokenExtractor,userExtractor}

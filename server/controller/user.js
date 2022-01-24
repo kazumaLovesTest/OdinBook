cb8e5c857d0b7374324a9e6764a35881b6c8f8a0
kazumaneet <mekbibtare@gmail.com>
2022-01-24 20:01:14 +0300

    implemented the route for handling friend request

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,7 +1,7 @@
 const User = require('../model/user')
 const userRoute = require('express').Router()
 const bcrypt = require('bcrypt')
-
+const middleware = require('../utils/middleware')
 
 userRoute.post('/', (req, res, next) => {
   const body = req.body
@@ -13,7 +13,7 @@ userRoute.post('/', (req, res, next) => {
 
   const saltRound = 10
   const passwordHash = bcrypt.hashSync(body.password, saltRound)
-  
+
   const user = new User({
     name: body.name,
     username: body.username,
@@ -26,4 +26,19 @@ userRoute.post('/', (req, res, next) => {
 
 })
 
+userRoute.post('/friend_request', middleware.userExtractor, async (req, res,next) => {
+  const body = req.body
+  const user = req.user
+
+  const userToSendRequestTo = await User.findOne(body)
+
+  userToSendRequestTo.friendRequest = userToSendRequestTo.friendRequest.concat(user._id)
+
+  await userToSendRequestTo.save()
+
+  res.status(201).end()
+
+  next()
+
+})
 module.exports = userRoute

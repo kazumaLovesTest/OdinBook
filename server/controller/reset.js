692f86b0e5711a596815dad4421fadfb4f27a55a
kazumaneet <mekbibtare@gmail.com>
2022-01-28 10:25:59 +0300

    Delete resetRoute

--- server/controller/reset.js
+++
@@ -1,9 +0,0 @@
-const resetRoute = require('express').Router()
-const User = require('../model/user')
-
-resetRoute.delete('/', async (req,res) => {
-  await User.deleteMany({})
-  res.status(204).end()
-})
-
-module.exports = resetRoute
